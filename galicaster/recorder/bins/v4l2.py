a2fc2d0581a432bda00b3226529bf53a9b2c1a54
Rich Wareham <rjw57@cam.ac.uk>
2019-01-30 12:45:33 +0000

    recorder/bins/v4l2: disable videobox in pipeline
    The videobox element appears to introduce artefacts when used with an
    epiphan video capture device[1]. Work around this by removing the
    videobox element from the pipeline.
    
    [1] https://github.com/teltek/Galicaster/issues/531

--- galicaster/recorder/bins/v4l2.py
+++ galicaster/recorder/bins/v4l2.py
@@ -18,7 +18,7 @@ from gi.repository import Gst
 from galicaster.recorder import base
 from galicaster.recorder.utils import get_videosink
 
-pipestr = (' v4l2src name=gc-v4l2-src ! capsfilter name=gc-v4l2-filter ! queue ! gc-v4l2-dec ! videobox name=gc-v4l2-videobox top=0 bottom=0 !'
+pipestr = (' v4l2src name=gc-v4l2-src ! capsfilter name=gc-v4l2-filter ! queue ! gc-v4l2-dec !'
            ' videorate ! videoconvert ! capsfilter name=gc-v4l2-vrate ! videocrop name=gc-v4l2-crop ! gc-videofilter ! '
            ' tee name=gc-v4l2-tee ! queue ! caps-preview ! gc-vsink '
            ' gc-v4l2-tee. ! queue ! valve drop=false name=gc-v4l2-valve ! videoconvert ! queue ! '
@@ -184,13 +184,10 @@ class GCv4l2(Gst.Bin, base.Base):
         src1.send_event(event)
 
     def disable_input(self):
-        src1 = self.get_by_name('gc-v4l2-videobox')
-        src1.set_properties(top = -10000, bottom = 10000)
+        pass
 
     def enable_input(self):
-        src1 = self.get_by_name('gc-v4l2-videobox')
-        src1.set_property('top',0)
-        src1.set_property('bottom',0)
+        pass
 
     def disable_preview(self):
         src1 = self.get_by_name('sink-'+self.options['name'])
